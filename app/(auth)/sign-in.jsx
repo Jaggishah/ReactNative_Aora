a0d27166ed6b31921884f095aec8137a7648d000
Jaggishah <jugdeepshah@gmail.com>
2024-05-12 14:21:09 -0700

    added some logs

--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -1,10 +1,11 @@
-import { View, Text, ScrollView, Image } from 'react-native'
+import { View, Text, ScrollView, Image, Alert } from 'react-native'
 import React, { useState } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { images } from "../../constants";
 import FormField from '../../components/FormField';
 import  CustomButton  from "../../components/customButton";
-import { Link } from 'expo-router';
+import { Link, router } from 'expo-router';
+import { signIn } from '../../lib/appwrite';
 
 const SignIn = () => {
   const [ form, setForm ] = useState({
@@ -13,11 +14,31 @@ const SignIn = () => {
   });
   const [ isSubmitting, setIsSubmitting ] = useState(false);
 
-  const submit = () => {}
+  const submit = async () => {
+
+    if( !form["email"] || !form["password"]){
+      Alert.alert('Error', 'Please fill in all the fields')
+    }
+    setIsSubmitting(true);
+
+    try{
+        
+        await signIn(form["email"], form["password"],form["username"]);
+
+        router.replace('/home');
+    }catch(err){
+
+      Alert.alert('Error', err.message);
+    }finally{
+      setIsSubmitting(false);
+    }
+    
+  }
+
   return (
     <SafeAreaView className="bg-primary h-full ">
       <ScrollView>
-        <View className="w-full justify-center h-full px-4 my-6">
+        <View className="w-full justify-center min-h-[86vh] px-4 my-6">
           <Image 
             source={images.logo}
             resizeMode="contain"
